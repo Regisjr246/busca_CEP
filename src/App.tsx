e4bc92d06334d687b2891794d7b75d094caaed7d
Regisjr246 <regisjr246@gmail.com>
2023-08-31 15:22:39 -0300

    react

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from './components/Heater';
+import Footer from './components/Footer'
+import BuscaCep from './components/BuscaCepForm';
+import styles from "./App.module.css"
+
+
+function App() {
+  return (
+   <div>
+<Header/>
+
+<main className={styles.main}>
+
+  <BuscaCep/>
+
+</main>
+
+
+<Footer/>
+
+   </div>
+  );
+}
+
+export default App;
+
---
+++ src/interfaces/CepInterface.ts
@@ -0,0 +1,9 @@
+export interface CepInterface{
+
+
+cep:string;
+localidade:string;
+uf:string;
+
+
+}
